5ea372f8edda56e554b5f816969b48826ae124f6
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2026-03-09 22:27:44 +0000

    added F1 fonts, and an extra fetch request for driver and constructor data

--- public/script.js
+++ public/script.js
@@ -1,12 +1,23 @@
 console.log("Script loaded");
 
-async function fetchData() {
+async function fetchDriverConstructors() {
+    try {
+        const response = await fetch("/api/drivers-and-constructors");
+        const data = await response.json();
+
+        console.log("Data from /api/drivers-and-constructors:", data);
+
+        displayDriverConstructorData(data);
+
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+async function fetchLeagueData() {
     try {
         const response = await fetch("/api/league");
         const data = await response.json();
-
-        document.getElementById("output").textContent =
-            JSON.stringify(data, null, 2);
 
         displayLeagueData(data)
 
@@ -15,35 +26,58 @@ async function fetchData() {
     }
 }
 
+function displayDriverConstructorData(data) {
+
+    // console.log(data)
+
+    const drivers = data.Data.driver;
+    const constructors = data.Data.constructor;
+
+    console.log({ drivers, constructors });
+
+    const driverConstructorList = document.getElementById("driverConstructorList");
+
+}
+
 function displayLeagueData(data) {
 
 
-    document.getElementById("output").innerHTML = '';
+    document.getElementById("leagueList").innerHTML = '';
     document.getElementById("lastUpdated").textContent = 'Last League Update: ' + data.FeedTime.CESTTime;
 
-    const teamName = decodeURIComponent(data.Value.leaderboard[0].team_name);
-
     const players = data.Value.leaderboard;
+
+    console.log({ players });
 
     players.forEach(player => {
         const playerName = decodeURIComponent(player.user_name);
         const teamName = decodeURIComponent(player.team_name);
         const points = player.cur_points;
-        console.log(`Player: ${playerName}, Team: ${teamName}, Points: ${points}`);
+        const playerTeam = player.user_team;
         const card = document.createElement('div');
         card.className = 'card mb-5';
         card.innerHTML = `
-            <div class="card-content">
-                <h3 class="title is-4">${playerName}</h3>
-                <h4 class="subtitle is-6">Team: ${teamName}</h4>
-                <p>Points: ${points}</p>
+            <div class="card-content is-flex is-flex-direction-row is-align-items-center is-gap-4">
+            <figure class="image is-128x128">
+            <img class="is-rounded" src="https://bulma.io/assets/images/placeholders/128x128.png" />
+            </figure>
+                <div>
+                    <h3 class="title is-4">${playerName}</h3>
+                    <h4 class="subtitle is-6">Team: ${teamName}</h4>
+                    <p>Points: ${points}</p>
+                    <p>Constructors: ${playerTeam[0]}, ${playerTeam[1]}</p>
+                    <p>Drivers: ${playerTeam[2]}, ${playerTeam[3]}, ${playerTeam[4]}, ${playerTeam[5]}, ${playerTeam[6]}</p>
+
+                </div>
             </div>
         `;
-        document.getElementById("output").appendChild(card);
+        document.getElementById("leagueList").appendChild(card);
     })
 
     d
 
 }
 
-fetchData() 
+fetchDriverConstructors();
+
+fetchLeagueData() 
--- server.js
+++ server.js
@@ -5,6 +5,21 @@ const PORT = 3000;
 
 app.use(express.static("public"));
 
+app.get("/api/drivers-and-constructors", async (req, res) => {
+    try {
+        const response = await fetch(
+            "https://fantasy.formula1.com/feeds/v2/statistics/driverconstructors_4.json"
+        );
+
+        const data = await response.json();
+        console.log("Fetched data from F1 API:", data);
+
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to fetch data" });
+    }
+});
+
 app.get("/api/league", async (req, res) => {
     try {
         const response = await fetch(

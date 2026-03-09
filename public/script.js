f1ac15bf358a67f015b80fa535fafe78e3a44d5a
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2026-03-09 21:22:47 +0000

    started outputting more data onto the page, added some bulma classes

--- public/script.js
+++ public/script.js
@@ -5,12 +5,45 @@ async function fetchData() {
         const response = await fetch("/api/league");
         const data = await response.json();
 
-        console.log(data);
-
         document.getElementById("output").textContent =
             JSON.stringify(data, null, 2);
+
+        displayLeagueData(data)
 
     } catch (error) {
         console.error("Error:", error);
     }
 }
+
+function displayLeagueData(data) {
+
+
+    document.getElementById("output").innerHTML = '';
+    document.getElementById("lastUpdated").textContent = 'Last League Update: ' + data.FeedTime.CESTTime;
+
+    const teamName = decodeURIComponent(data.Value.leaderboard[0].team_name);
+
+    const players = data.Value.leaderboard;
+
+    players.forEach(player => {
+        const playerName = decodeURIComponent(player.user_name);
+        const teamName = decodeURIComponent(player.team_name);
+        const points = player.cur_points;
+        console.log(`Player: ${playerName}, Team: ${teamName}, Points: ${points}`);
+        const card = document.createElement('div');
+        card.className = 'card mb-5';
+        card.innerHTML = `
+            <div class="card-content">
+                <h3 class="title is-4">${playerName}</h3>
+                <h4 class="subtitle is-6">Team: ${teamName}</h4>
+                <p>Points: ${points}</p>
+            </div>
+        `;
+        document.getElementById("output").appendChild(card);
+    })
+
+    d
+
+}
+
+fetchData() 
